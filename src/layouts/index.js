b909e99a8ee76f69daf01a1468f675c89a214edf
joel <joelbitar@protonmail.com>
2018-01-03 00:30:13 -0500

    Linting Index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import '../stylesheets/main.scss'
+import '../stylesheets/main.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Joel Hoelting"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Joel Hoelting: Full Stack Web Developer' },
       ]}
     />
     <div className="container">
       {children()}
     </div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
