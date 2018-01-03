57e2069ee6ffefecafdc16ef8854402771a3ee36
joel <joelbitar@protonmail.com>
2018-01-02 21:12:07 -0500

    Added ESLint Config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+// import Link from 'gatsby-link'
 import Logo from '../components/Logo';
 
-const IndexPage = () => (
-  <div className="flex-container">
-    <Logo />
-  </div>
-)
+class IndexPage extends React.Component {
+  // constructor() {
+  //   super();
+  //
+  //   this.handleKeyPress = this.handleKeyPress.bind(this);
+  //
+  //   this.state = {
+  //     input: [],
+  //   };
+  // }
 
-export default IndexPage
+  // handleKeyPress(event) {
+  //   const secretCode = 'jghjgh';
+  //   const input = [...this.state.input];
+  //   input.push(event.key);
+  //   input.splice(-secretCode.length - 1, input.length - secretCode.length);
+  //
+  //   if (input.join('').includes(secretCode)) {
+  //     console.log('DING DING!');
+  //   }
+  //   this.setState({input}, function() {
+  //     console.log(this.state.input);
+  //   });
+  // }
+
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.handleKeyPress);
+  // }
+
+  render() {
+    return (
+      <div className="flex-container">
+        <Logo />
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
