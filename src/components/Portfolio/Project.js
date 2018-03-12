f483f933158dab99fe62b0a7660a80ccd04cedcd
Joel Hoelting <joelhoelting@protonmail.com>
2018-03-11 21:52:42 -0400

    Typo on Portfolio Page

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Project = (props) => (
  <div>
   <a href={props.url} target="_blank"><img src={props.image} /></a>
-  <h1>{props.title}</h1>
+  <a href={props.url} target="_blank"><h1>{props.title}</h1></a>
   <h3>{props.description}</h3>
   <h6>Tools: {props.tools.join(' ')}</h6>
  </div>
